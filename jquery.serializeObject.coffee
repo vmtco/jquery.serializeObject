08292fff4fa925bf8cf2ce5fda733748974b8cbc
vmtco <vmtco.com@gmail.com>
2015-04-24 08:35:56 +0430

    optimize all code

--- jquery.serializeObject.coffee
+++ jquery.serializeObject.coffee
@@ -1,14 +1,14 @@
 ( ($) ->
     $.fn.serializeObject = ->
-        objects = {};
-        a = this.serializeArray()
-        $.each(a, ->
+        objects = {}
+        items = this.serializeArray()
+        $.each items, ->
             if objects[this.name] isnt undefined
                 unless objects[this.name].push
                     objects[this.name] = [objects[this.name]]
                 objects[this.name].push(this.value or "")
             else
                 objects[this.name] = this.value or ""
-        )
         objects
+    return
 )(jQuery)
--- jquery.serializeObject.js
+++ jquery.serializeObject.js
@@ -1,23 +1,18 @@
-(function() {
-  (function($) {
-    return $.fn.serializeObject = function() {
-      var a, objects;
-      objects = {};
-      a = this.serializeArray();
-      $.each(a, function() {
-        if (objects[this.name] !== void 0) {
-          if (!objects[this.name].push) {
-            objects[this.name] = [objects[this.name]];
-          }
-          return objects[this.name].push(this.value || "");
-        } else {
-          return objects[this.name] = this.value || "";
+(function($) {
+  $.fn.serializeObject = function() {
+    var items, objects;
+    objects = {};
+    items = this.serializeArray();
+    $.each(items, function() {
+      if (objects[this.name] !== void 0) {
+        if (!objects[this.name].push) {
+          objects[this.name] = [objects[this.name]];
         }
-      });
-      return objects;
-    };
-  })(jQuery);
-
-}).call(this);
-
-//# sourceMappingURL=jquery.serializeObject.js.map
+        return objects[this.name].push(this.value || "");
+      } else {
+        return objects[this.name] = this.value || "";
+      }
+    });
+    return objects;
+  };
+})(jQuery);
